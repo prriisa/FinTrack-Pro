f662a9fb91ca73d9270f8abb4a1c9f63710e55ef
PRIYA <priyasharma45567@gmail.com>
2026-07-08 09:56:29 +0530

    feature-1 completed

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+const toDoList = document.querySelector('.to-do-list')
 const form = document.querySelector('form')
 const task = document.querySelector('#task')
 const taskDisc = document.querySelector('#task-desc')
@@ -5,7 +6,8 @@ const category = document.querySelector('.category')
 const important = document.querySelector('.important')
 const date = document.querySelector('#date')
 const taskContainer = document.querySelector('.task-container')
-const taskDiv = document.querySelectorAll('.task-div')
+const dateContainer = document.querySelector(".date-container")
+const backBtn = document.querySelector('.back')
 
 let data = (JSON.parse(localStorage.getItem('data')) || [])
 
@@ -16,12 +18,11 @@ function addTask(data) {
         let dataDiv = document.querySelector(`.data.${obj.category}`);
         if (dataDiv) {
             dataDiv.innerHTML += `
-                    <div class="task-div ${obj.date}" data-index="${index}">
+                    <div class="task-div ${obj.important} ${obj.date}" data-index="${index}" data-date="${obj.date}">
                         <div class="checkbox">
-                            <div class="checkbox-tick" style="display: ${obj.checkBox ? 'block' : 'none'}"></div>
+                            <div class="checkbox-tick ${obj.important}" style="display: ${obj.checkBox ? 'block' : 'none'}"></div>
                         </div>
-                        <p style="text-decoration: ${obj.checkBox ? 'line-through' : 'none'}">${obj.task}</p>
-                        <div class="description">${obj.discription}</div>
+                        <p style="text-decoration: ${obj.checkBox ? 'line-through' : 'none'};color: ${obj.checkBox ? 'rgb(137, 137, 137)' : 'rgb(70, 70, 70)'};">${obj.task}</p>                        <div class="description">${obj.discription}</div>
                         <div class="dlt" data-index="${index}"><i class="ri-delete-bin-fill" style="color:rgb(38, 38, 38)"></i></div>
                     </div>`
 
@@ -29,12 +30,11 @@ function addTask(data) {
             taskContainer.innerHTML += `
                 <div class="data ${obj.category}">
                     <h6>${obj.category}</h6>
-                    <div class="task-div ${obj.date}" data-index="${index}">
+                    <div class="task-div ${obj.important} ${obj.date}" data-index="${index}" data-date="${obj.date}">
                         <div class="checkbox">
                             <div class="checkbox-tick ${obj.important}" style="display: ${obj.checkBox ? 'block' : 'none'}"></div>
                         </div>
-                        <p style="text-decoration: ${obj.checkBox ? 'line-through' : 'none'}">${obj.task}</p>
-                        <div class="description">${obj.discription}</div>
+                        <p style="text-decoration: ${obj.checkBox ? 'line-through' : 'none'};color: ${obj.checkBox ? 'rgb(137, 137, 137)' : 'rgb(70, 70, 70)'};">${obj.task}</p>                        <div class="description">${obj.discription}</div>
                         <div class="dlt" data-index="${index}"><i class="ri-delete-bin-fill" style="color:rgb(38, 38, 38)"></i></div>
                     </div>
                 </div>`
@@ -46,28 +46,35 @@ addTask(data)
 
 function addDate(data) {
 
-    const dateContainer = document.querySelector(".date-container");
     dateContainer.innerHTML = "";
 
     const days = ["SUN", "MON", "TUES", "WED", "THU", "FRI", "SAT"];
 
     const addedDates = new Set();
 
+    if (dateContainer.innerHTML === '') {
+        dateContainer.innerHTML += `
+            <div class="task-date selected" data-date="All">
+                <h6>ALL</h6>
+            </div>
+            `
+    }
+
     data.forEach((obj) => {
 
         if (addedDates.has(obj.date)) return;
 
         addedDates.add(obj.date);
-
         const inputDate = new Date(obj.date);
 
-        dateContainer.innerHTML += `
-            <div class="task-date">
+        dateContainer.innerHTML +=
+            `
+            <div class="task-date" data-date="${obj.date}">
                 <h6>${days[inputDate.getDay()]}</h6>
                 <h6>${inputDate.getDate()}</h6>
             </div>
-        `;
-    });
+            `
+    })
 }
 addDate(data)
 
@@ -88,11 +95,9 @@ form.addEventListener('submit', function (e) {
         important: important.value
     })
 
-
     localStorage.setItem('data', JSON.stringify(data));
 
     addTask(data)
-
     addDate(data)
 
     form.reset()
@@ -107,6 +112,7 @@ taskContainer.addEventListener('click', function (e) {
         localStorage.setItem('data', JSON.stringify(data))
         addTask(data)
         addDate(data)
+        return
     }
 
     if (e.target.closest('.checkbox')) {
@@ -121,4 +127,37 @@ taskContainer.addEventListener('click', function (e) {
         addTask(data);
         addDate(data);
     }
+})
+
+dateContainer.addEventListener('click', (e) => {
+    const currDiv = e.target.closest('.task-date');
+    if (!currDiv) return
+
+    document.querySelectorAll('.task-date').forEach((div) => {
+        div.classList.remove('selected')
+    })
+
+    currDiv.classList.add('selected')
+    const taskDivs = document.querySelectorAll('.task-div')
+    const selectedDate = currDiv.dataset.date
+
+    taskDivs.forEach((tdiv) => {
+        tdiv.style.display = (selectedDate === 'All' || tdiv.dataset.date === selectedDate) ? 'flex' : 'none'
+    });
+
+    document.querySelectorAll('.data').forEach((div) => {
+        const td = div.querySelectorAll('.task-div');
+        let sabHidden = true
+        for (let i = 0; i < td.length; i++) {
+            if (td[i].style.display !== 'none') {
+                sabHidden = false
+                break
+            }
+        }
+        div.style.display = sabHidden ? 'none' : 'flex'
+    });
+})
+
+backBtn.addEventListener('click', () => {
+    toDoList.style.display = 'none'
 })
